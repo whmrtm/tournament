2895ed064610156426e0e9221eff5d2c5290fc61
whmrtm <whmrtm@gmail.com>
2016-11-08 16:42:20 -0500

    second commit

--- tournament.sql
+++ tournament.sql
@@ -1,3 +1,7 @@
+drop database if exists tournament
+create database tournament
+\c tournament
+
 create table player
 (
 p_id SERIAL,
@@ -8,7 +12,9 @@ primary key(p_id)
 create table match
 (
 match_id SERIAL,
-win_id INT NOT NULL,
-lost_id INT NOT NULL,
+win_id INT NOT NULL references player(p_id),
+lost_id INT NOT NULL references player(p_id),
 primary key(match_id)
 );
+
+
